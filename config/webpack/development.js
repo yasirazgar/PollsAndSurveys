f3353fd51725ab88cf8ffe77e14269f563517d50
yasirazgar <yasirazgar@gmail.com>
2018-11-21 18:23:50 +0530

    configure sass compilation

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,13 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+environment.loaders.insert('sass', {
+    test: /\.scss$/,
+    use: [
+        "style-loader", // creates style nodes from JS strings
+        "css-loader", // translates CSS into CommonJS
+        "sass-loader" // compiles Sass to CSS
+    ]
+});
+
 module.exports = environment.toWebpackConfig()
