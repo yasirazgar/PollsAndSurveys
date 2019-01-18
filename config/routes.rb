07c10ee30f2b74229286f9f25832cc2805cca26d
yasirazgar <yasirazgar@gmail.com>
2019-01-18 17:16:47 +0530

    add session routes for api and web

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  namespace :v1 do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    get "signin", to: "sessions#new", as: "signin"
+    post "login", to: "sessions#create", as: "login"
+    get "logout", to: "sessions#destroy", as: "logout"
+    get "signup", to: "users#new", as: "signup"
+    resources :users, only: [:create, :update, :edit]
+    root to: "home#index"
+  end
+
+  get "signin", to: "sessions#new", as: "signin"
+  post "login", to: "sessions#create", as: "login"
+  get "logout", to: "sessions#destroy", as: "logout"
+  get "signup", to: "users#new", as: "signup"
+  resources :users, only: [:create, :update, :edit]
   root to: "home#index"
 end
