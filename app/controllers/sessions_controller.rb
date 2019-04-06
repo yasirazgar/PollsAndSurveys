a47091b4c7f3861daa07c440aeff861006b33125
yasirazgar <yasirazgar@gmail.com>
2019-04-06 23:24:26 +0530

    fix sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,15 +7,15 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      render json: {user: user, message: "Successfully logged in"}
+      render json: {user: user, message: "Successfully logged in"}, status: :ok
     else
-      render json: {user: nil, message: "Invalid email or password"}
+      render json: {user: nil, message: "Invalid email or password"}, status: :not_found
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: "Logged out!"
+    render json: {message: "Successfully logged out"}, status: :ok
   end
 
 end
