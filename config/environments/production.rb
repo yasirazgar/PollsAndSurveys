317394064d5e41db13130f1343171d656f4500e9
yasirazgar <yasirazgar@gmail.com>
2019-04-06 23:49:14 +0530

    update app to rails 5.3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,4 @@
 Rails.application.configure do
-  # Verifies that versions and hashed value of the package contents in the project's package.json
-  config.webpacker.check_yarn_integrity = false
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -63,7 +61,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "PollsAndSurveys_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "polls_and_surveys_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
