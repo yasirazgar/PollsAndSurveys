8c69824260f777aef2f64f377088fbd2658dfee7
yasirazgar <yasirazgar@gmail.com>
2019-04-19 23:36:16 +0530

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,60 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = User.create(nick_name: 'collector', name: 'Yasir', email: 'yasir@gmail.com', password: '@Rac123')
+user1 = User.create(nick_name: 'azzu', name: 'Azgar', email: 'azgar@gmail.com', password: '@Rac123')
+user2 = User.create(nick_name: 'hawk', name: 'Mohammed', email: 'mohammed@gmail.com', password: '@Rac123')
+
+cat1 = Category.create(name: 'Software', aliases: ['It', 'Information Technology'])
+cat2 = Category.create(name: 'Software Development', aliases: ['It', 'Information Technology', 'Software'])
+cat3 = Category.create(name: 'politics', aliases: ['Elections'])
+cat3 = Category.create(name: 'Gk')
+
+opt1 = Option.create(option: 'Java')
+opt2 = Option.create(option: 'JavaScript')
+opt3 = Option.create(option: 'Ruby')
+opt4 = Option.create(option: 'Python')
+opt5 = Option.create(option: 'Cobra')
+opt6 = Option.create(option: 'Rattle')
+opt7 = Option.create(option: 'Black mamba')
+opt8 = Option.create(option: 'Cat')
+opt9 = Option.create(option: 'Dog')
+
+poll1 = Poll.new(question: 'Your fav language', user_id: admin.id)
+poll1.categories << cat1
+poll1.categories << cat2
+poll1.save
+
+poll1_opt1 = PollsOptions.create(poll_id: poll1.id, option_id: opt1.id)
+poll1_opt2 = PollsOptions.create(poll_id: poll1.id, option_id: opt2.id)
+poll1_opt3 = PollsOptions.create(poll_id: poll1.id, option_id: opt3.id)
+poll1_opt4 = PollsOptions.create(poll_id: poll1.id, option_id: opt4.id)
+poll1_ans1 = PollAnswer.create(user_id: admin.id, polls_options_id: poll1_opt1.id)
+poll1_ans2 = PollAnswer.create(user_id: user1.id, polls_options_id: poll1_opt2.id)
+poll1_ans1 = PollAnswer.create(user_id: user2.id, polls_options_id: poll1_opt3.id)
+
+poll2 = Poll.create(question: 'Longest Snake', category_ids: [cat3.id], user_id: user1.id)
+poll2.categories << cat3
+poll1.save
+
+poll2_opt1 = PollsOptions.create(poll_id: poll2.id, option_id: opt4.id)
+poll2_opt2 = PollsOptions.create(poll_id: poll2.id, option_id: opt5.id)
+poll2_opt3 = PollsOptions.create(poll_id: poll2.id, option_id: opt6.id)
+poll2_opt4 = PollsOptions.create(poll_id: poll2.id, option_id: opt7.id)
+poll2_ans1 = PollAnswer.create(user_id: admin.id, polls_options_id: poll2_opt1.id)
+poll2_ans2 = PollAnswer.create(user_id: user1.id, polls_options_id: poll2_opt2.id)
+poll2_ans3 = PollAnswer.create(user_id: user2.id, polls_options_id: poll2_opt3.id)
+
+poll3 = Poll.create(question: 'What do you like the most', user_id: user2.id)
+
+poll3_opt1 = PollsOptions.create(poll_id: poll3.id, option_id: opt3.id)
+poll3_opt2 = PollsOptions.create(poll_id: poll3.id, option_id: opt5.id)
+poll3_opt3 = PollsOptions.create(poll_id: poll3.id, option_id: opt8.id)
+poll3_opt4 = PollsOptions.create(poll_id: poll3.id, option_id: opt9.id)
+poll3_ans1 = PollAnswer.create(user_id: admin.id, polls_options_id: poll3_opt1.id)
+poll3_ans2 = PollAnswer.create(user_id: user1.id, polls_options_id: poll3_opt2.id)
+poll3_ans3 = PollAnswer.create(user_id: user2.id, polls_options_id: poll3_opt3.id)
+
+
+
