ee8f5bdd2e62fc1802d3ff703b86e6d8f75993ca
yasirazgar <yasirazgar@gmail.com>
2019-05-20 22:55:13 +0530

    remove unwanted sessions_controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  get "signin", to: "sessions#new", as: "signin"
   post "login", to: "sessions#create", as: "login"
   get "logout", to: "sessions#destroy", as: "logout"
-  get "signup", to: "users#new", as: "signup"
   resources :users, only: [:create, :destroy, :update]
   resource :user, controller: :user, only: [] do
     collection do
