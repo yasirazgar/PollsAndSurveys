6ca0951257e2ce56a5ea0678c82059a3707bd99a
yasirazgar <yasirazgar@gmail.com>
2019-05-20 22:51:16 +0530

    fix users controller and its test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
   def create
+    user = User.create(create_params)
+    if user
+      render json: {name: user.name, message: 'Regsitration successfully'}
+    else
+      render json: {message: 'Error in Regsitration' + user.errors.full_messages.join('')}, status: :bad_request
+    end
   end
 
   def destroy
+    User.destroy(params[:id])
+    render json: {message: 'User destroyed'}
+  end
+
+  private
+
+  def create_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :email, :name, :nick_name)
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,27 +1,27 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-
   test "create" do
-    params = {
-      email: 'yasir@pas.com',
-      password: 'Password@1',
-      password_confirmation: 'Password@1',
-      name: 'Yasir'
+    user_params = {
+      user:{
+        email: 'someuser@pas.com',
+        password: 'Password@1',
+        password_confirmation: 'Password@1',
+        name: 'Yasir',
+        nick_name: 'Yasir'
+      }
     }
     assert_difference('User.count', 1) do
-      post(users_url, params: params, xhr: true)
+      post(users_url, params: user_params, xhr: true)
     end
-
-    user = User.find_by_email(user_params[:email])
-    assert_equal(user_params[:name], user.name)
+    user = User.find_by_email(user_params[:user][:email])
+    assert_equal(user_params[:user][:name], user.name)
+    assert_equal(user_params[:user][:nick_name], user.nick_name)
   end
 
   test "destroy" do
-    user = users(:yasir)
-
     assert_difference('User.count', -1) do
-      delete(user_url(user), xhr: true)
+      delete(user_url(users(:david)), xhr: true)
     end
   end
 end
