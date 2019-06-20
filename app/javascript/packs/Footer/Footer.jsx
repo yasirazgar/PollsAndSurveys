3eb491ccf60a5307aabfef8864819b761b4f1b58
yasirazgar <yasirazgar@gmail.com>
2019-06-21 00:24:07 +0530

    add local select to footer

--- app/javascript/packs/Footer/Footer.jsx
+++ app/javascript/packs/Footer/Footer.jsx
@@ -1,38 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 
 import FooterLeft from './FooterLeft'
 import FooterRight from './FooterRight'
-import Select from '../Utils/Select'
-import { buildTranslations } from '../../actions'
 
 import './Footer.scss'
 
-class Footer extends Component {
+const Footer = props => (
+  <div className={"footer " + props.classes}>
+    <FooterRight />
+    <FooterLeft />
 
-  changeHandler = event => {
-    debugger
-    this.props.buildTranslations(event.target.value)
-  }
-
-  render(){
-    return(
-      <div className={"footer " + this.props.classes}>
-        <FooterRight />
-        <FooterLeft />
-
-        <div>
-          Surveys coming soon
-          <Select options={[['en', 'en'], ['ta', 'ta']]} onChange={this.changeHandler}/>
-        </div>
-      </div>
-    )
-  }
-}
+    <div>
+      Surveys coming soon
+    </div>
+  </div>
+)
 
 Footer.propTypes = {
   classes: PropTypes.string
 };
 
-export default connect(null, {buildTranslations})(Footer)
+export default Footer
--- app/javascript/packs/Footer/FooterLeft.jsx
+++ app/javascript/packs/Footer/FooterLeft.jsx
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import InstaIcon from 'instagram.svg'
 import FbIcon from 'facebook.svg'
 import TwitterIcon from 'twitter.svg'
+import Select from '../Utils/Select'
+import { buildTranslations } from '../../actions'
 
+class FooterLeft extends Component {
+  changeHandler = event => {
+    this.props.buildTranslations(event.target.value)
+  }
+  render(){
+    return(
+      <span className="left">
+        <div className="tooltip">
+          <img src={InstaIcon}></img>
+          <span className="tooltiptext">Tooltip text</span>
+        </div>
+        <div className="tooltip">
+          <img src={FbIcon}></img>
+          <span className="tooltiptext">Tooltip text</span>
+        </div>
+        <div className="tooltip">
+          <img src={TwitterIcon}></img>
+          <span className="tooltiptext">Tooltip text</span>
+        </div>
+        <div>
+          <Select options={[['en', 'en'], ['ta', 'ta']]} onChange={this.changeHandler}/>
+        </div>
+      </span>
+    )
+  }
+}
 
-const FooterLeft = props => (  
-  <span className="left">
-    <div className="tooltip">
-      <img src={InstaIcon}></img>
-      <span className="tooltiptext">Tooltip text</span>
-    </div>
-    <div className="tooltip">
-      <img src={FbIcon}></img>
-      <span className="tooltiptext">Tooltip text</span>
-    </div>
-    <div className="tooltip">
-      <img src={TwitterIcon}></img>
-      <span className="tooltiptext">Tooltip text</span>
-    </div>
-  </span>
-);
-
-export default FooterLeft
+export default connect(null, {buildTranslations})(FooterLeft)
