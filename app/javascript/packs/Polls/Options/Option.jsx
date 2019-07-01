e32949d7a4d6bbfeeb71e95531bc97b027bec8b7
yasirazgar <yasirazgar@gmail.com>
2019-07-01 21:35:03 +0530

    focus and flash the sign button on trying to answer the poll without singing in

--- app/javascript/packs/Polls/Options/Option.jsx
+++ app/javascript/packs/Polls/Options/Option.jsx
@@ -3,12 +3,23 @@ import PropTypes from 'prop-types';
 import answerPoll from '../../Handlers/answerPollHandler';
 
 class Option extends Component {
+
+  flashSignInButton = () => {
+    const button = document.getElementsByClassName("signin")[0]
+    button.focus()
+    button.classList.add("flash")
+    setTimeout(() => { button.classList.remove("flash"); }, 3000);
+  }
+
   render(){
     const option_id = this.props.option.option_id;
     const user = JSON.parse(window.localStorage.getItem('user'));
     let clickHandler
     if (user){
       clickHandler = answerPoll.bind(this, this.props.pollId, option_id, this.props.callback);
+    }
+    else{
+      clickHandler = this.flashSignInButton
     }
 
     return (
