717dad4e16e23e9b2aed750092c44f746ee4109d
yasirazgar <yasirazgar@gmail.com>
2019-07-04 22:14:17 +0530

    fix jest test

--- test/javascript/setupTests.js
+++ test/javascript/setupTests.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Enzyme from 'enzyme'
+import Enzyme from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import { createSerializer } from 'enzyme-to-json';
 
