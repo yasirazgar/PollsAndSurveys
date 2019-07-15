504c7d906eda6635e11d543bdeeadfd2cdbea4f5
yasirazgar <yasirazgar@gmail.com>
2019-07-15 22:17:42 +0530

    add missed file authentication controller

---
+++ test/controllers/authentication_controller_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class AuthenticationControllerTest < ActionDispatch::IntegrationTest
+  test "create - login - success" do
+    user = users(:yasir)
+    params = {
+      email: 'yasir@pas.com',
+      password: 'passworD%1'
+    }
+
+    post(login_url, params: params, xhr: true)
+
+    assert_response :success
+
+    assert_equal(user.slice(:name, :nick_name, :email), json_response['user'])
+    assert_equal('Successfully logged in', json_response['message'])
+    assert_not_nil(json_response['jwt'])
+  end
+
+  test "create - login - failure" do
+    user = users(:yasir)
+    params = {
+      email: 'yasir@pas.com',
+      password: 'PassworD@1'
+    }
+
+    post(login_url, params: params, xhr: true)
+
+    assert_response :not_found
+
+    assert_equal('Username or password incorrect', json_response['error'])
+
+    assert_nil(json_response['jwt'])
+  end
+end
