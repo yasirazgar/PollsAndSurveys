24c859db972ff0647497a4920e1f79c90899a059
yasirazgar <yasirazgar@gmail.com>
2019-07-27 12:58:02 +0530

    fix users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,18 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_request, only: [:create]
   def create
     user = User.create(create_params)
-    if user
+
+    if user.errors.blank?
       render json: {name: user.name, message: 'Regsitration successfully'}
     else
-      render json: {message: 'Error in Regsitration' + user.errors.full_messages.join('')}, status: :bad_request
+      render json: {error: 'Error in Regsitration' + user.errors.full_messages.join('')}, status: :bad_request
     end
   end
 
   def destroy
     User.destroy(params[:id])
+
     render json: {message: 'User destroyed'}
   end
 
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -19,9 +19,39 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_equal(user_params[:user][:nick_name], user.nick_name)
   end
 
+  test "create - Error" do
+    yasir = users(:yasir)
+    user_params = {
+      user:{
+        email: yasir.email,
+        password: 'Password@1',
+        password_confirmation: 'Password@1',
+        name: 'Yasir',
+        nick_name: 'Yasir'
+      }
+    }
+    assert_difference('User.count', 0) do
+      post(users_url, params: user_params, xhr: true)
+    end
+
+    assert_response :bad_request
+
+    assert_equal("Error in RegsitrationEmail has already been taken", json_response['error'])
+  end
+
   test "destroy" do
+    yasir = users(:yasir)
+
     assert_difference('User.count', -1) do
-      delete(user_url(users(:david)), xhr: true)
+      delete(user_url(users(:david)), headers: { "Authorization" => token_for_user(yasir) }, xhr: true)
     end
   end
+
+  # test "destroy - should not allow to destroy self" do
+  #   yasir = users(:yasir)
+
+  #   assert_difference('User.count', 0) do
+  #     delete(users_url(yasir), headers: { "Authorization" => token_for_user(yasir) }, xhr: true)
+  #   end
+  # end
 end
